9e2ca845609cbf37a0fecbf49b108ca281e98423
Jaime Takikawa, Marina Poltorak and Nathan Aden <marina@>
2019-04-08 13:57:20 -0700

    write fill function to stapler

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,46 @@ function stapler(color, maxStaples){
 }
 
 stapler.prototype.staple = function(doubleStaple) {
+  // check if double staple
   if (doubleStaple === true) {
+    // check if 2 or more staples
     if (this.currentStaples >= 2) {
+      // if two or more remove 2 and return remaining staples.
       this.currentStaples -= 2;
       return this.currentStaples;
     } else {
+      // if not 2 or more staples return false
       return false;
     }
 
+
+
   } else {
-    this.currentStaples--;
+    // check if single staple
+    if (this.currentStaples >= 1) {
+      // if single staple remove 1,remaining staples
+      this.currentStaples -= 1;
+      return this.currentStaples;
+    } else {
+      // if out of staples, return false
+      return false;
+    }
   }
 };
 
+stapler.prototype.fill = function(amount) {
+   debugger;
+  if (amount === undefined) {
+    this.currentStaples = this.maxStaples / 2;
+  }
+   if (amount >=0) {
+    this.currentStaples += amount
+  }
+  if (this.currentStaples > this.maxStaples) {
+    this.currentStaples = this.maxStaples;
+  }
+  return this.currentStaples
+}
+// fill stapler to max max staples
 
 var bigStapler = new stapler("blue", 10);
