e7a2d918cd1a2d7a49f2375d2769f67c54ea00fe
Jaime Takikawa, Marina Poltorak and Nathan Aden <jaime+marina+nathan@>
2019-04-08 12:00:49 -0700

    create branch for practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,24 @@
-
-function Destination(name, location, landmarks, season, notes) {
-  this.name = name;
-  this.location = location;
-  this.landmarks = landmarks;
-  this.season = season;
-  this.notes = notes;
+function stapler(color, maxStaples){
+  this.staplerColor = color;
+  this.maxStaples = maxStaples;
+  this.currentStaples = maxStaples / 2;
+  this.isOwn = true;
+  this.users = [];
 }
 
-Destination.prototype.fullDestination = function() {
-  return this.name + ", " + this.location + ", " + this.landmarks + ", " + this.season + ", " + this.notes;
-}
+stapler.prototype.staple = function(doubleStaple) {
+  if (doubleStaple === true) {
+    if (this.currentStaples >= 2) {
+      this.currentStaples -= 2;
+      return this.currentStaples;
+    } else {
+      return false;
+    }
 
-function myPlaceList() {
-  this.places = [];
-}
-myPlaceList.prototype.addPlace = function(thing) {
-  this.places.push(thing);
-}
-
-
-var myPlaces;
-
-$(document).ready(function () {
-  myPlaces = new myPlaceList();
-  myPlaces.addPlace(new Destination("Paris", "France", "Eiffel Tower", "Spring", "Went to a jazz club, Drank wine"));
-  myPlaces.addPlace(new Destination("Dublin", "Ireland", "Blarny Stone", "Summer", "Went to a Pub, Drank beer"));
-  myPlaces.addPlace(new Destination("Bangkok", "Thailand", "Food market", "Summer", "don't take a moto bike taxi"));
+  } else {
+    this.currentStaples--;
+  }
+};
 
 
-});
+var bigStapler = new stapler("blue", 10);
